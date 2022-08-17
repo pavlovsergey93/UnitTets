f63d5ef5acf54dce786cc6ed1fb5cd3ac42931b4
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-08-17 15:58:51 +0300

    Тесты для DetailsPresenter

---
+++ app/src/test/java/com/geekbrains/tests/presenter/details/DetailsPresenterTest.kt
@@ -0,0 +1,79 @@
+package com.geekbrains.tests.presenter.details
+
+import android.os.Build
+import androidx.lifecycle.Lifecycle
+import androidx.test.core.app.ActivityScenario
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.geekbrains.tests.view.details.DetailsActivity
+import com.geekbrains.tests.view.details.ViewDetailsContract
+import com.nhaarman.mockito_kotlin.times
+import com.nhaarman.mockito_kotlin.verify
+import org.junit.After
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers.eq
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.robolectric.annotation.Config
+
+@RunWith(AndroidJUnit4::class)
+@Config(sdk = [Build.VERSION_CODES.O_MR1])
+class DetailsPresenterTest {
+	@Mock
+	private lateinit var presenter: DetailsPresenter
+
+	@Mock
+	private lateinit var viewContract: ViewDetailsContract
+
+	private var countMock: Int = 10
+
+	@Before
+	fun setUp() {
+		MockitoAnnotations.openMocks(this)
+	}
+
+	@Test
+	fun setCounter_Test() {
+		presenter.setCounter(countMock)
+		verify(presenter).setCounter(eq(10))
+	}
+
+	@Test
+	fun onIncrement_Test() {
+		viewContract.setCount(++countMock)
+		verify(viewContract).setCount(eq(11))
+
+	}
+
+	@Test
+	fun onDecrement_Test() {
+		viewContract.setCount(--countMock)
+		verify(viewContract).setCount(eq(9))
+	}
+
+	@Test
+	fun onAttach_Test() {
+		presenter.onAttach(viewContract)
+		val instance = presenter.javaClass
+		instance.declaredFields.forEach {
+			it.isAccessible = true
+			if (it.name == "view") {
+				Assert.assertEquals(viewContract, it.get(presenter))
+			}
+		}
+	}
+
+	@Test
+	fun onDetach_Test(){
+		presenter.onDetach()
+		val instance = presenter.javaClass
+		instance.declaredFields.forEach {
+			it.isAccessible = true
+			if (it.name == "view") {
+				Assert.assertNull(it.get(presenter))
+			}
+		}
+	}
+}
