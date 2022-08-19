4b4e69299c69ea72c4657ef4e628cbc4c7dd6ebf
Pavlov Sergey <pavlov.androdev@gmail.com>
2022-08-19 13:31:53 +0300

    дополнительный материал к уроку

--- app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
+++ app/src/main/java/com/geekbrains/tests/view/search/MainActivity.kt
@@ -6,10 +6,13 @@ import android.view.inputmethod.EditorInfo
 import android.widget.TextView.OnEditorActionListener
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.geekbrains.tests.BuildConfig
 import com.geekbrains.tests.R
 import com.geekbrains.tests.model.SearchResult
+import com.geekbrains.tests.presenter.RepositoryContract
 import com.geekbrains.tests.presenter.search.PresenterSearchContract
 import com.geekbrains.tests.presenter.search.SearchPresenter
+import com.geekbrains.tests.repository.FakeGitHubRepository
 import com.geekbrains.tests.repository.GitHubApi
 import com.geekbrains.tests.repository.GitHubRepository
 import com.geekbrains.tests.view.details.DetailsActivity
@@ -63,8 +66,12 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
         })
     }
 
-    private fun createRepository(): GitHubRepository {
-        return GitHubRepository(createRetrofit().create(GitHubApi::class.java))
+    private fun createRepository(): RepositoryContract {
+		return if (BuildConfig.TYPE == FAKE) {
+			FakeGitHubRepository()
+		} else {
+			GitHubRepository(createRetrofit().create(GitHubApi::class.java))
+		}
     }
 
     private fun createRetrofit(): Retrofit {
@@ -100,6 +107,7 @@ class MainActivity : AppCompatActivity(), ViewSearchContract {
 
     companion object {
         const val BASE_URL = "https://api.github.com"
+		const val FAKE = "FAKE"
     }
 
 	override fun onDestroy() {
